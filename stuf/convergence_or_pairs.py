93f8be174cb76d22eb5f8118763ab59ee48b0f2a
vi7n <singhrob@oregonstate.edu>
2024-12-18 09:28:50 +0200

    del new filw

--- stuf/convergence_or_pairs.py
+++ stuf/convergence_or_pairs.py
@@ -48,23 +48,6 @@ final_value = (
     cash + smci_position * smci_data.iloc[-1] + nvda_position * nvda_data.iloc[-1]
 )
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 # Plot stock prices
 plt.figure(figsize=(10, 6))
 plt.plot(smci_pct, label="SMCI")
